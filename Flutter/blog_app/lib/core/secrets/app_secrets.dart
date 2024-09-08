b482c6fa5c43a3ed0e85f3b49216d3b6b37c28c8
ashutosh229 <akumarjha875@gmail.com>
2024-09-09 05:27:22 +0530

    changes done

---
+++ Flutter/blog_app/lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = "https://zgdywhzsvlvcqczsjwsj.supabase.co";
+  static const supabaseAnonKey =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpnZHl3aHpzdmx2Y3FjenNqd3NqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU1NDE2ODIsImV4cCI6MjA0MTExNzY4Mn0.XD0mxHi8xhVTEStwp8uYEyllJWH5LciLcaI94Nhy90Q";
+}
---
+++ Flutter/blog_app/lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -0,0 +1,46 @@
+import 'package:blog_app/core/error/failures.dart';
+import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/domain/entities/user.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDataSource remoteDataSource;
+
+  AuthRepositoryImpl(this.remoteDataSource);
+
+  @override
+  Future<Either<Failure, User>> loginWithEmailPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final user = await remoteDataSource.loginWithEmailPassword(
+          email: email, password: password);
+      return right(user);
+    } catch (error) {
+      return left(Failure("Login Failed: ${error.runtimeType}"));
+    }
+  }
+
+  @override
+  Future<Either<Failure, User>> signUpWithEmailPassword(
+      {required String name,
+      required String email,
+      required String password}) async {
+    return _getUser(
+      () async => await remoteDataSource.signUpWithEmailPassword(name: name, email: email, password: password);
+    );
+  }
+
+  Future<Either<Failure, User>> _getUser(
+    Future<User> Function() fn,
+  ) async {
+    try {
+      final user = await fn();
+      return right(user);
+    } catch (error) {
+      throw left(Failure("Could not get the user due to some issues"));
+    }
+  }
+}
